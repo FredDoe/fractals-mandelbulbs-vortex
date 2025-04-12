718a25ec6905f620019589f929c002faeaccf38d
Godfred Doe <godfredoe9@gmail.com>
2025-04-12 16:19:11 +0800

    removed every simulation from the implementation directory purposely to segregate them

--- implementations/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Some numerical computation implementations for the course project sits here.
-taichi: https://docs.taichi-lang.org/
-https://github.com/taichi-dev/taichi/tree/master/python/taichi/examples/simulation
-"""
